01ec90d5934b0ebf33dceb81a832f19b97229a76
Victor Da Silva <victor.dasilva@pep80.fr>
2025-12-04 11:14:06 +0100

    [+] Ajout du menu dans la fenêtre du jeu

--- main.c
+++ main.c
@@ -8,9 +8,61 @@
 #define INIT_WIDTH 800
 #define INIT_HEIGHT 800
 
-/**
- * @brief Projette un point 3D sur un écran 2D
- */
+// ==========================================
+// OUTILS DESSIN & TEXTE (PIXEL ART)
+// ==========================================
+
+void drawChar(SDL_Renderer* renderer, int x, int y, int scale, char c) {
+    int patterns[][15] = {
+        {1,1,1, 1,0,1, 1,0,1, 1,0,1, 1,1,1}, // 0
+        {0,1,0, 0,1,0, 0,1,0, 0,1,0, 0,1,0}, // 1
+        {1,1,1, 0,0,1, 1,1,1, 1,0,0, 1,1,1}, // 2
+        {1,1,1, 0,0,1, 1,1,1, 0,0,1, 1,1,1}, // 3
+        {1,0,1, 1,0,1, 1,1,1, 0,0,1, 0,0,1}, // 4
+        {1,1,1, 1,0,0, 1,1,1, 0,0,1, 1,1,1}, // 5
+        {1,1,1, 1,0,0, 1,1,1, 1,0,1, 1,1,1}, // 6
+        {1,1,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1}, // 7
+        {1,1,1, 1,0,1, 1,1,1, 1,0,1, 1,1,1}, // 8
+        {1,1,1, 1,0,1, 1,1,1, 0,0,1, 1,1,1}, // 9
+        {1,1,1, 1,0,0, 1,1,1, 0,0,1, 1,1,1}, // S (Start)
+        {1,0,1, 1,1,1, 1,0,1, 1,0,1, 1,0,1}, // M (Mode)
+        {1,1,1, 0,1,0, 0,1,0, 0,1,0, 0,1,0}, // T (Taux)
+        {1,1,1, 1,0,1, 1,1,0, 1,0,1, 1,0,1}, // R (Random)
+        {1,1,1, 1,0,0, 1,0,1, 1,0,1, 1,1,1}, // G (Glider)
+    };
+
+    int index = -1;
+    if (c >= '0' && c <= '9') index = c - '0';
+    if (c == 'S') index = 10;
+    if (c == 'M') index = 11;
+    if (c == 'T') index = 12;
+    if (c == 'R') index = 13;
+    if (c == 'G') index = 14;
+
+    if (index >= 0) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (patterns[index][i * 3 + j]) {
+                    SDL_Rect pixel = {x + j * scale, y + i * scale, scale, scale};
+                    SDL_RenderFillRect(renderer, &pixel);
+                }
+            }
+        }
+    }
+}
+
+void drawNumber(SDL_Renderer* renderer, int x, int y, int scale, int number) {
+    char buffer[10];
+    sprintf(buffer, "%d", number);
+    for (int i = 0; buffer[i] != '\0'; i++) {
+        drawChar(renderer, x + (i * 4 * scale), y, scale, buffer[i]);
+    }
+}
+
+// ==========================================
+// FONCTIONS JEU
+// ==========================================
+
 void drawCube(SDL_Renderer *renderer, int grid[N][N][N], float angleX, float angleY,
               float currentScale, float camX, float camY, int winW, int winH) {
 
@@ -26,8 +78,8 @@ void drawCube(SDL_Renderer *renderer, int grid[N][N][N], float angleX, float ang
             for (int k = 0; k < N; k++) {
 
                 int age = grid[i][j][k];
+                // On affiche QUE les cellules vivantes
                 if (age > 0) {
-                    // --- 1. CALCULS 3D ---
                     float x = (i - offset) * currentScale;
                     float y = (j - offset) * currentScale;
                     float z = (k - offset) * currentScale;
@@ -46,8 +98,10 @@ void drawCube(SDL_Renderer *renderer, int grid[N][N][N], float angleX, float ang
 
                     int screenX = (int)(cx + x2 * z_factor + camX);
                     int screenY = (int)(cy + y2 * z_factor + camY);
+                    int size = (int)(currentScale * 0.8f * z_factor);
+                    if (size < 1) size = 1;
 
-                    // --- 2. COULEUR (Age + Profondeur) ---
+                    // CELLULE VIVANTE (Blanc -> Rouge)
                     int green_blue_val = 255 - (age * 5);
                     if (green_blue_val < 0) green_blue_val = 0;
 
@@ -60,10 +114,6 @@ void drawCube(SDL_Renderer *renderer, int grid[N][N][N], float angleX, float ang
                     Uint8 b = (Uint8)(green_blue_val * depthShade);
 
                     SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-
-                    int size = (int)(currentScale * 0.8f * z_factor);
-                    if (size < 1) size = 1;
-
                     SDL_Rect voxel = { screenX, screenY, size, size };
                     SDL_RenderFillRect(renderer, &voxel);
                 }
@@ -98,159 +148,215 @@ void nextGeneration3D(int grid[N][N][N], int nextGrid[N][N][N], int x, int y, in
     }
 }
 
+// ==========================================
+// FONCTIONS DU MENU GRAPHIQUE
+// ==========================================
+void drawMenu(SDL_Renderer* renderer, int winW, int winH, int selectedMode, int taux) {
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+    SDL_RenderClear(renderer);
+
+    // --- BOUTON MODE 1 (RANDOM) ---
+    SDL_Rect btn1 = {winW/4 - 100, winH/3, 200, 150};
+    if (selectedMode == 1) SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    else SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+    SDL_RenderDrawRect(renderer, &btn1);
+
+    for(int i=0; i<50; i++) {
+        SDL_RenderDrawPoint(renderer, btn1.x + rand()%200, btn1.y + rand()%150);
+    }
+    drawChar(renderer, btn1.x + 80, btn1.y + 160, 5, 'R');
+
+
+    // --- BOUTON MODE 2 (GLIDER) ---
+    SDL_Rect btn2 = {3*winW/4 - 100, winH/3, 200, 150};
+    if (selectedMode == 2) SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    else SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+    SDL_RenderDrawRect(renderer, &btn2);
+
+    // Icone Glider
+    SDL_Rect gPiece = {btn2.x + 80, btn2.y + 60, 40, 40};
+    SDL_RenderFillRect(renderer, &gPiece);
+    // Texte "G"
+    drawChar(renderer, btn2.x + 80, btn2.y + 160, 5, 'G');
+
+
+    // --- SLIDER TAUX (Si mode Random) ---
+    if (selectedMode == 1) {
+        SDL_Rect barBg = {winW/4, 2*winH/3, winW/2, 20};
+        SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+        SDL_RenderFillRect(renderer, &barBg);
+
+        SDL_Rect barFill = {winW/4, 2*winH/3, (winW/2) * taux / 100, 20};
+        SDL_SetRenderDrawColor(renderer, 200, 50, 50, 255);
+        SDL_RenderFillRect(renderer, &barFill);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        drawChar(renderer, winW/4 - 40, 2*winH/3 - 5, 5, 'T');
+        drawNumber(renderer, 3*winW/4 + 20, 2*winH/3 - 5, 5, taux);
+    }
+
+    // --- BOUTON START ---
+    SDL_Rect btnStart = {winW/2 - 100, 4*winH/5, 200, 60};
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawRect(renderer, &btnStart);
+    drawChar(renderer, btnStart.x + 90, btnStart.y + 15, 6, 'S');
+
+    SDL_RenderPresent(renderer);
+}
+
+
 int main(int argc, char* argv[]) {
 
-    int mode = 0;
-    int taux = 20;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) return 1;
 
-    printf("--- JEU DE LA VIE 3D (Vitesse Variable) ---\n");
-    printf("CONTROLES :\n");
-    printf("  [Espace]  : Pause/Play\n");
-    printf("  [E]       : ACCELERER (Reduit le delai)\n");
-    printf("  [A]       : RALENTIR (Augmente le delai)\n");
-    printf("  [F11]     : Plein Ecran\n");
-    printf("  [Fleches] : Rotation\n");
-    printf("  [I/K/J/L] : Deplacement\n");
-    printf("  [+ / -]   : Zoom\n\n");
+    SDL_Window* window = SDL_CreateWindow("Jeu 3D - Menu Graphique",
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        INIT_WIDTH, INIT_HEIGHT,
+                                        SDL_WINDOW_RESIZABLE | SDL_WINDOW_FULLSCREEN_DESKTOP);
 
-    printf("Choix (1: Aleatoire, 2: Glider): ");
-    if (scanf("%d", &mode) != 1) mode = 1;
-    while(getchar() != '\n');
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    int isFullscreen = 1;
+    int programRunning = 1;
 
     static int grid[N][N][N];
     static int nextGrid[N][N][N];
 
-    if (mode == 1) {
-        printf("Taux (%%): ");
-        scanf("%d", &taux);
-        srand(time(0));
-        for(int i=0; i<N; i++)
-            for(int j=0; j<N; j++)
-                for(int k=0; k<N; k++)
-                    grid[i][j][k] = (rand() % 100 < taux) ? 1 : 0;
-    } else {
-        for(int i=0; i<N; i++)
-            for(int j=0; j<N; j++)
-                for(int k=0; k<N; k++)
-                    grid[i][j][k] = 0;
-        int c = N/2;
-        grid[c][c][c]=1; grid[c+1][c][c]=1; grid[c][c+1][c]=1; grid[c][c][c+1]=1; grid[c-1][c][c]=1;
-    }
+    // --- BOUCLE GLOBALE ---
+    while (programRunning) {
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) return 1;
+        // --- 1. PHASE MENU ---
+        int inMenu = 1;
+        int selectedMode = 1;
+        int taux = 20;
+        int winWidth, winHeight;
+        SDL_GetWindowSize(window, &winWidth, &winHeight);
 
-    SDL_Window* window = SDL_CreateWindow("Jeu 3D - Vitesse A/E",
-                                        SDL_WINDOWPOS_CENTERED,
-                                        SDL_WINDOWPOS_CENTERED,
-                                        INIT_WIDTH, INIT_HEIGHT,
-                                        SDL_WINDOW_RESIZABLE);
+        while (inMenu && programRunning) {
+            SDL_Event e;
+            while (SDL_PollEvent(&e)) {
+                if (e.type == SDL_QUIT) programRunning = 0;
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+                if (e.type == SDL_WINDOWEVENT && (e.window.event == SDL_WINDOWEVENT_RESIZED)) {
+                    winWidth = e.window.data1;
+                    winHeight = e.window.data2;
+                }
 
-    int winWidth = INIT_WIDTH;
-    int winHeight = INIT_HEIGHT;
+                if (e.type == SDL_KEYDOWN) {
+                    if (e.key.keysym.sym == SDLK_ESCAPE) programRunning = 0;
+                    if (e.key.keysym.sym == SDLK_LEFT) selectedMode = 1;
+                    if (e.key.keysym.sym == SDLK_RIGHT) selectedMode = 2;
+                    if (e.key.keysym.sym == SDLK_UP && taux < 100) taux += 5;
+                    if (e.key.keysym.sym == SDLK_DOWN && taux > 5) taux -= 5;
+                    if (e.key.keysym.sym == SDLK_RETURN || e.key.keysym.sym == SDLK_KP_ENTER) inMenu = 0;
+                }
 
-    int running = 1;
-    int paused = 1;
-    int generation = 0;
-    int isFullscreen = 0;
+                if (e.type == SDL_MOUSEBUTTONDOWN) {
+                    int mx = e.button.x;
+                    int my = e.button.y;
 
-    int generationDelay = 50; // Délai initial
+                    if (mx > winWidth/4 - 100 && mx < winWidth/4 + 100 && my > winHeight/3 && my < winHeight/3 + 150)
+                        selectedMode = 1;
+                    if (mx > 3*winWidth/4 - 100 && mx < 3*winWidth/4 + 100 && my > winHeight/3 && my < winHeight/3 + 150)
+                        selectedMode = 2;
+                    if (mx > winWidth/2 - 100 && mx < winWidth/2 + 100 && my > 4*winHeight/5 && my < 4*winHeight/5 + 60)
+                        inMenu = 0;
 
-    float angleX = 0.0f;
-    float angleY = 0.0f;
-    float currentScale = 10.0f;
-    float camX = 0.0f;
-    float camY = 0.0f;
+                    if (selectedMode == 1 && my > 2*winHeight/3 && my < 2*winHeight/3 + 20) {
+                        int relX = mx - (winWidth/4);
+                        if (relX >= 0 && relX <= winWidth/2) {
+                            taux = (relX * 100) / (winWidth/2);
+                        }
+                    }
+                }
+            }
 
-    Uint32 lastGenTime = SDL_GetTicks();
+            drawMenu(renderer, winWidth, winHeight, selectedMode, taux);
+            SDL_Delay(16);
+        }
 
-    while (running) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) running = 0;
+        if (!programRunning) break;
 
-            if (event.type == SDL_WINDOWEVENT) {
-                if (event.window.event == SDL_WINDOWEVENT_RESIZED ||
-                    event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+        // --- 2. INITIALISATION JEU ---
+        int paused = 1;
+
+        if (selectedMode == 1) {
+            // MODE RANDOM
+            srand(time(0));
+            for(int i=0; i<N; i++) for(int j=0; j<N; j++) for(int k=0; k<N; k++)
+                grid[i][j][k] = (rand() % 100 < taux) ? 1 : 0;
+        } else {
+            // MODE GLIDER
+            for(int i=0; i<N; i++) for(int j=0; j<N; j++) for(int k=0; k<N; k++) grid[i][j][k] = 0;
+            int c = N/2;
+            grid[c][c][c]=1; grid[c+1][c][c]=1; grid[c][c+1][c]=1; grid[c][c][c+1]=1; grid[c-1][c][c]=1;
+        }
+
+        int generationDelay = 50;
+        float angleX = 0.0f;
+        float angleY = 0.0f;
+        float currentScale = 10.0f;
+        float camX = 0.0f; float camY = 0.0f;
+        int generation = 0;
+
+        Uint32 lastGenTime = SDL_GetTicks();
+        SDL_GetWindowSize(window, &winWidth, &winHeight);
+
+        // --- 3. BOUCLE JEU ---
+        int gameRunning = 1;
+        while (gameRunning && programRunning) {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) programRunning = 0;
+
+                if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
                     winWidth = event.window.data1;
                     winHeight = event.window.data2;
                 }
-            }
 
-            if (event.type == SDL_KEYDOWN) {
-                switch(event.key.keysym.sym) {
-                    case SDLK_q: running = 0; break;
-                    case SDLK_SPACE: paused = !paused; break;
-
-                    case SDLK_F11:
-                    case SDLK_f:
-                        isFullscreen = !isFullscreen;
-                        SDL_SetWindowFullscreen(window, isFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
-                        SDL_GetWindowSize(window, &winWidth, &winHeight);
-                        break;
-
-                    // --- GESTION DE LA VITESSE (E / A) ---
-                    case SDLK_e: // Plus vite (E)
-                        generationDelay -= 10;
-                        if (generationDelay < 1) generationDelay = 1;
-                        break;
-                    case SDLK_a: // Moins vite (A)
-                        generationDelay += 10;
-                        break;
-
-                    // Caméra & Zoom
-                    case SDLK_LEFT: angleY -= 0.1f; break;
-                    case SDLK_RIGHT: angleY += 0.1f; break;
-                    case SDLK_UP: angleX -= 0.1f; break;
-                    case SDLK_DOWN: angleX += 0.1f; break;
-                    case SDLK_r:
-                        angleX = 0; angleY = 0;
-                        currentScale = 10.0f;
-                        camX = 0; camY = 0;
-                        generationDelay = 50;
-                        break;
-
-                    case SDLK_l: camX -= 20.0f; break;
-                    case SDLK_j: camX += 20.0f; break;
-                    case SDLK_i: camY += 20.0f; break;
-                    case SDLK_k: camY -= 20.0f; break;
-
-                    case SDLK_KP_PLUS:
-                    case SDLK_PLUS:
-                        currentScale += 1.0f;
-                        break;
-                    case SDLK_KP_MINUS:
-                    case SDLK_MINUS:
-                        currentScale -= 1.0f;
-                        if (currentScale < 1.0f) currentScale = 1.0f;
-                        break;
+                if (event.type == SDL_KEYDOWN) {
+                    switch(event.key.keysym.sym) {
+                        case SDLK_ESCAPE: programRunning = 0; break;
+                        case SDLK_BACKSPACE: gameRunning = 0; break; // Retour Menu
+                        case SDLK_SPACE: paused = !paused; break;
+                        case SDLK_F11:
+                            isFullscreen = !isFullscreen;
+                            SDL_SetWindowFullscreen(window, isFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
+                            SDL_GetWindowSize(window, &winWidth, &winHeight);
+                            break;
+                        case SDLK_e: generationDelay = (generationDelay > 1) ? generationDelay - 10 : 1; break;
+                        case SDLK_a: generationDelay += 10; break;
+                        case SDLK_LEFT: angleY -= 0.1f; break;
+                        case SDLK_RIGHT: angleY += 0.1f; break;
+                        case SDLK_UP: angleX -= 0.1f; break;
+                        case SDLK_DOWN: angleX += 0.1f; break;
+                        case SDLK_r: angleX=0; angleY=0; currentScale=10.0f; camX=0; camY=0; break;
+                        case SDLK_l: camX -= 20.0f; break;
+                        case SDLK_j: camX += 20.0f; break;
+                        case SDLK_i: camY += 20.0f; break;
+                        case SDLK_k: camY -= 20.0f; break;
+                        case SDLK_PLUS: case SDLK_KP_PLUS: currentScale += 1.0f; break;
+                        case SDLK_MINUS: case SDLK_KP_MINUS: if(currentScale > 1) currentScale -= 1.0f; break;
+                    }
                 }
             }
+
+            if (!paused && SDL_GetTicks() - lastGenTime > generationDelay) {
+                for(int i=0; i<N; i++) for(int j=0; j<N; j++) for(int k=0; k<N; k++) nextGeneration3D(grid, nextGrid, i, j, k);
+                for(int i=0; i<N; i++) for(int j=0; j<N; j++) for(int k=0; k<N; k++) grid[i][j][k] = nextGrid[i][j][k];
+                generation++;
+                lastGenTime = SDL_GetTicks();
+            }
+
+            drawCube(renderer, grid, angleX, angleY, currentScale, camX, camY, winWidth, winHeight);
+
+            char title[100];
+            snprintf(title, 100, "Gen: %d | [BACK] MENU | [SPACE] PAUSE", generation);
+            SDL_SetWindowTitle(window, title);
+
+            SDL_Delay(16);
         }
-
-        if (!paused && SDL_GetTicks() - lastGenTime > generationDelay) {
-            for(int i=0; i<N; i++)
-                for(int j=0; j<N; j++)
-                    for(int k=0; k<N; k++)
-                        nextGeneration3D(grid, nextGrid, i, j, k);
-
-            for(int i=0; i<N; i++)
-                for(int j=0; j<N; j++)
-                    for(int k=0; k<N; k++)
-                        grid[i][j][k] = nextGrid[i][j][k];
-
-            generation++;
-            lastGenTime = SDL_GetTicks();
-        }
-
-        drawCube(renderer, grid, angleX, angleY, currentScale, camX, camY, winWidth, winHeight);
-
-        char title[100];
-        snprintf(title, 100, "Gen: %d | Delai: %dms | Zoom: %.1f", generation, generationDelay, currentScale);
-        SDL_SetWindowTitle(window, title);
-
-        SDL_Delay(16);
     }
 
     SDL_DestroyRenderer(renderer);
